e1cf7a600dd52a4d9319740f24a9e0683ef2d206
Rodrigo <rodrigosuch@gmail.com>
2018-09-12 21:08:15 -0300

    Created packet analyser module

---
+++ include/PacketAnalyzer.h
@@ -0,0 +1,19 @@
+/*
+ * PacketAnalyzer.h
+ *
+ *  Created on: 9 de set de 2018
+ *      Author: rodrigo
+ */
+
+#ifndef PACKETANALYZER_H_
+#define PACKETANALYZER_H_
+
+class PacketAnalyzer
+{
+public:
+
+  pcap_handler PckAnlz_GetPacketReceivedCallback(void);
+
+};
+
+#endif /* PACKETANALYZER_H_ */
