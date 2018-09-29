e23ee640ff71b7b93b25a1ac8b6fc99e2fd4873a
Rodrigo <rodrigosuch@gmail.com>
2018-09-29 20:49:48 -0300

    Including build file

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -4,20 +4,26 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/TCPAnalyse.cpp 
+../src/PCAPLib.cpp \
+../src/PacketAnalyzer.cpp \
+../src/main.cpp 
 
 OBJS += \
-./src/TCPAnalyse.o 
+./src/PCAPLib.o \
+./src/PacketAnalyzer.o \
+./src/main.o 
 
 CPP_DEPS += \
-./src/TCPAnalyse.d 
+./src/PCAPLib.d \
+./src/PacketAnalyzer.d \
+./src/main.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -I../include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
